c5bc83e0bd6a15de3787dad85af919d3a4643b16
hyf <hyf@Ubuntu20.myguest.virtualbox.org>
2023-06-30 22:45:18 +0800

    add disk read func

--- src/boot.asm
+++ src/boot.asm
@@ -20,8 +20,83 @@ xchg bx, bx;
 mov si, booting_str
 call print
 
+mov edi, 0x1000 ;读取硬盘块到目标内存
+mov ecx, 0      ;起始扇区
+mov bl, 1       ;扇区数量
+
+call read_disk
+
+;bochs 魔术断点，使程序停在此处
+
 ; 阻塞
 jmp $
+
+read_disk:
+  mov dx, 0x1f2
+  mov al, bl    ;out指令只接受al和dx寄存器
+  out dx, al    ;将al内容写到dx代表的设备寄存器中
+
+  inc dx  ;0x1f3
+  mov al, cl
+  out dx, al
+
+  inc dx  ;0x1f4
+  shr ecx, 8
+  mov al, cl
+  out dx, al
+
+  inc dx  ;0x1f5
+  shr ecx, 8
+  mov al, cl
+  out dx, al
+
+  inc dx  ;0x1f6
+  shr ecx, 8
+  and cl, 0b1111
+
+  mov al, 0b1110_0000
+  or al, cl
+  out dx, al
+
+  inc dx  ;0x1f7
+  mov al, 0x20
+  out dx, al
+
+  xor ecx, ecx
+  mov cl, bl    ;得到读写扇区的数量
+
+  .read:
+    push cx
+    call .waits ;等待数据准备完成
+    call .reads ;读取一个扇区
+    pop cs
+    loop .read
+  ret
+
+  .waits:
+    mov dx, 0x1f7
+    .check:
+      in al, dx ;从0x1f7端口读取状态   
+      nop
+      nop
+      nop
+      and al, 0b1000_1000
+      cmp al, 0b0000_1000
+      jnz .check
+    ret
+  
+  .reads:
+    mov dx, 0x1f0
+    mov cx, 256
+    .readw:
+      in ax, dx ;从0x1f0读取一个字
+      nop
+      nop
+      nop
+      mov [edi], ax
+      add edi, 2
+      loop .readw
+    ret
 
 print:
   mov ah, 0x0e
