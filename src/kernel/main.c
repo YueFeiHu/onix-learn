db10cb8b522e08a7e494b8b89bc27124a168b64e
hyf <1208549329@qq.com>
2023-07-08 22:25:54 +0800

    add memory map init

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -6,16 +6,18 @@ extern void time_init();
 extern void rtc_init();
 extern void hang();
 extern void set_alarm(u32);
+extern void memory_map_init();
+extern void memory_test();
 
 void kernel_init()
 {
-    console_init();
-    gdt_init();
+    
+    memory_map_init();
     interrupt_init();
     clock_init();
-    time_init();
-    rtc_init();
-    set_alarm(2);
+    memory_test();
+    // time_init();
+    // rtc_init();
     // task_init();
     asm volatile("sti");
     hang();
--- src/kernel/memory.c
+++ src/kernel/memory.c
@@ -2,6 +2,8 @@
 #include <onix/types.h>
 #include <onix/debug.h>
 #include <onix/assert.h>
+#include <onix/stdlib.h>
+#include <onix/string.h>
 
 #define LOGK(fmt, args...) DEBUGK(fmt, ##args)
 
@@ -9,6 +11,8 @@
 #define ZONE_RESERVED 2 // ards 不可用区域
 
 #define IDX(addr) ((u32)addr >> 12)
+#define PAGE(idx) ((u32)idx << 12)
+#define ASSERT_PAGE(addr) assert((addr & 0xfff) == 0)
 
 typedef struct ards_t
 {
@@ -17,10 +21,10 @@ typedef struct ards_t
   u32 type;
 }_packed ards_t;
 
-u32 memory_base = 0;
-u32 memory_size = 0;
-u32 total_pages = 0;
-u32 free_pages  = 0;
+static u32 memory_base = 0;
+static u32 memory_size = 0;
+static u32 total_pages = 0;
+static u32 free_pages  = 0;
 
 #define used_pages (total_pages - free_pages)
 
@@ -59,4 +63,70 @@ void memory_init(u32 magic, u32 addr)
 
   LOGK("Total pages %d\n", total_pages);
   LOGK("Free pages %d\n", free_pages);
+}
+
+static u32 start_page = 0;
+static u8 *memory_map;
+static u32 memory_map_pages;
+
+void memory_map_init()
+{
+  memory_map = (u8*)memory_base;
+  // 计算物理内存数组需要占用的页数
+  memory_map_pages = div_round_up(total_pages, PAGE_SIZE);
+  LOGK("memory map page count %d\n", memory_map_pages);
+  free_pages -= memory_map_pages;
+  memset((void*)memory_map, 0, memory_map_pages * PAGE_SIZE);
+  start_page = IDX(MEMORY_BASE) + memory_map_pages;
+  for (size_t i = 0; i < start_page; i++)
+  {
+    memory_map[i] = 1;
+  }
+  LOGK("total pages %d free pages %d\n", total_pages, free_pages);
+}
+
+static u32 get_page()
+{
+  for (size_t i = start_page; i < total_pages; i++)
+  {
+    if (!memory_map[i])
+    {
+      memory_map[i] = 1;
+      free_pages--;
+      assert(free_pages >= 0);
+      u32 page = ((u32) i) << 12;
+      LOGK("get page 0x%p\n", page);
+      return page;
+    }
+  }
+  panic("OOM");
+}
+
+static void put_page(u32 addr)
+{
+  ASSERT_PAGE(addr);
+  u32 idx = IDX(addr);
+
+  assert(idx >= start_page && idx < total_pages);
+  assert(memory_map[idx] >= 1);
+  memory_map[idx]--;
+  if (!memory_map[idx])
+  {
+    free_pages++;
+  }
+  assert(free_pages > 0 && free_pages < total_pages);
+  LOGK("put pages 0x%p\n", addr);
+}
+
+void memory_test()
+{
+  u32 pages[10];
+  for (size_t i = 0;i < 10; i++)
+  {
+    pages[i] = get_page();
+  }
+  for (size_t i = 0;i < 10; i++)
+  {
+    put_page(pages[i]);
+  }
 }
