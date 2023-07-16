f6ae31574cf05d4c6d82e132eb5a7877a7c2ba8a
hyf <1208549329@qq.com>
2023-07-16 16:57:58 +0800

    add switch to user mode

--- src/kernel/thread.c
+++ src/kernel/thread.c
@@ -4,6 +4,7 @@
 #include <onix/task.h>
 #include <onix/mutex.h>
 #include <onix/printk.h>
+#include <onix/task.h>
 
 #define LOGK(fmt, args...) DEBUGK(fmt, ##args)
 
@@ -26,19 +27,21 @@ void idle_thread()
 
 extern u32 keyboard_read(char *buf, u32 count);
 
-void init_thread()
+static void real_init_thread()
 {
-    set_interrupt_state(true);
     u32 counter = 0;
     char ch;
     while (true)
     {
-        bool intr = interrupt_disable();
-        keyboard_read(&ch, 1);
-        printk("%c", ch);
-        set_interrupt_state(intr);
-        sleep(500);
+        sleep(100);
     }
+}
+
+void init_thread()
+{
+    // set_interrupt_state(true);
+    char temp[100]; // 为栈顶有足够的空间
+    task_to_user_mode(real_init_thread);
 }
 
 void test_thread()
